254cb9a8f65fea8da2a3685e6bf53077decc7e56
Tim Knapp <tknapp@qsciences.com>
2021-01-29 10:55:18 -0700

    add failing lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,8 @@
 module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended', //this plugin is required to use eslint for react, otherwise we will get errors for 'React' import, as 'no-unused-vars'
+  ],
   parser: 'babel-eslint',
   rules: {
     'react/jsx-filename-extension': 'off',
