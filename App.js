829a5479e1c27be960034f45e40ada3269e7c415
Tim Knapp <tknapp@qsciences.com>
2021-02-02 15:36:34 -0700

    add themes to status bar

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import { StatusBar } from 'react-native';
 import AppContext from './src/Contexts/AppContext';
 import { ThemeProvider } from 'styled-components/native';
 import { darkTheme } from './src/Styles/themes';
@@ -7,10 +7,14 @@ import LoginStack from './src/Navigation/LoginStack';
 import { NavigationContainer } from '@react-navigation/native';
 
 const App = () => {
+  const [theme, setTheme] = useState(darkTheme);
   return (
-    <ThemeProvider theme={darkTheme}>
-      <AppContext.Provider value={{ theme: darkTheme }}>
-        <StatusBar style="auto" />
+    <ThemeProvider theme={theme}>
+      <AppContext.Provider value={{ theme, setTheme }}>
+        <StatusBar
+          backgroundColor={theme.backgroundColor}
+          barStyle={theme.statusBar}
+        />
         <NavigationContainer>
           <LoginStack />
         </NavigationContainer>
