a3c3c1c71b768dc9999df7405f0015f859ec52ab
Tim Knapp <tknapp@qsciences.com>
2021-02-03 10:09:53 -0700

    set up proptypes for home screen

--- src/components/HomeScreen/index.js
+++ src/components/HomeScreen/index.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import { ScreenContainer, H2 } from '../Common';
+import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
+import { ScreenContainer, H2, PrimaryButton } from '../Common';
+import AppContext from '../../Contexts/AppContext';
 
-const HomeScreen = () => {
+const HomeScreen = ({ navigation }) => {
+  const { setIsSignedIn } = useContext(AppContext);
   return (
     <ScreenContainer>
       <H2>Home Screen</H2>
+      <PrimaryButton onPress={() => navigation.navigate('ProfileScreen')}>
+        See Profile
+      </PrimaryButton>
+      <PrimaryButton
+        style={{ marginTop: 12 }}
+        onPress={() => setIsSignedIn(false)}>
+        Sign out
+      </PrimaryButton>
     </ScreenContainer>
   );
+};
+
+HomeScreen.propTypes = {
+  navigation: PropTypes.object.isRequired,
 };
 
 export default HomeScreen;
