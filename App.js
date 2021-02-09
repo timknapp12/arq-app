5e15e9f543feed75af3ee2ebf8c880a991dc8fc3
Tim Knapp <tknapp@qsciences.com>
2021-02-09 14:13:14 -0700

    analytics and localization to app.js

--- App.js
+++ App.js
@@ -8,10 +8,17 @@ import Tabs from './src/Navigation/Tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import firebaseConfig from './firebase.config';
 import * as firebase from 'firebase';
+import * as Localization from 'expo-localization';
+import i18n from 'i18n-js';
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
 }
+
+// Set the locale once at the beginning of your app.
+i18n.locale = Localization.locale;
+// When a value is missing from a language it'll fallback to another language with the key present.
+i18n.fallbacks = true;
 
 const App = () => {
   const [theme, setTheme] = useState(darkTheme);
