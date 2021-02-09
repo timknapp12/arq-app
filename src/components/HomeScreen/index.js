f869db2617a6baf7045af5610f9b27cd122ba9a0
Tim Knapp <tknapp@qsciences.com>
2021-02-09 14:12:13 -0700

    firebase analytics to home screen

--- src/components/HomeScreen/index.js
+++ src/components/HomeScreen/index.js
@@ -1,12 +1,13 @@
 import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
-import { ScreenContainer, H2, PrimaryButton } from '../Common';
+import { ScreenContainer, H1, H2, PrimaryButton } from '../Common';
 import AppContext from '../../Contexts/AppContext';
 import * as Analytics from 'expo-firebase-analytics';
+import { Localized, init } from '../../Translations/Localized';
 
 const HomeScreen = ({ navigation }) => {
+  init();
   const { setIsSignedIn } = useContext(AppContext);
-
   const onPressProfile = () => {
     navigation.navigate('ProfileScreen');
     // source for analytics: https://docs.expo.io/versions/latest/sdk/firebase-analytics/
@@ -14,7 +15,7 @@ const HomeScreen = ({ navigation }) => {
       rank: 'Platinum',
     });
     Analytics.setUserId('test-user');
-    Analytics.logEvent('Tapped button to go to profile screen', {
+    Analytics.logEvent('Tapped_button_to_go_to_profile_screen', {
       screen: 'Home Screen',
       purpose: 'View personal profile',
     });
@@ -22,6 +23,7 @@ const HomeScreen = ({ navigation }) => {
 
   return (
     <ScreenContainer>
+      <H1 testID="home-screen-welcome">{Localized('welcome')}</H1>
       <H2 testID="home-screen-title">Home Screen</H2>
       <PrimaryButton testID="go-to-profile-button" onPress={onPressProfile}>
         See Profile
