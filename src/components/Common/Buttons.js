dc21526efa78593ef9a3d7a74e1779ff286882ce
Tim Knapp <tknapp@qsciences.com>
2021-02-10 13:58:30 -0700

    add new primary button

--- src/components/Common/Buttons.js
+++ src/components/Common/Buttons.js
@@ -1,27 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components/native';
-import { H4 } from './Texts';
+import { H2 } from './Texts';
 
 // source for themes with styled components: https://styled-components.com/docs/advanced#theming
 
-const InvertedH4 = styled(H4)`
-  color: ${(props) => props.theme.invertedColor};
-  text-align: center;
+const sharedCss = {
+  width: '100%',
+  height: 40,
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  borderRadius: 5,
+  borderWidth: 1,
+};
+
+// LOGIN BUTTON
+const ThemedButton = styled.TouchableOpacity`
+  ${sharedCss};
+  background-color: ${(props) =>
+    props.disabled
+      ? props.theme.disabledBackgroundColor
+      : props.theme.activeTint};
+  border-color: ${(props) =>
+    props.disabled
+      ? props.theme.disabledBackgroundColor
+      : props.theme.activeTint};
 `;
 
-const ThemedPrimaryButton = styled.TouchableOpacity`
-  background-color: ${(props) => props.theme.color};
-  padding: 8px;
-  width: 100%;
+const ThemedText = styled(H2)`
+  color: ${(props) => (props.disabled ? 'gray' : 'white')};
 `;
 
-export const PrimaryButton = ({ children = 'Button', ...props }) => (
-  <ThemedPrimaryButton {...props}>
-    <InvertedH4>{children}</InvertedH4>
-  </ThemedPrimaryButton>
+export const PrimaryButton = ({ disabled, children, ...props }) => (
+  <ThemedButton disabled={disabled} {...props}>
+    <ThemedText disabled={disabled}>{children}</ThemedText>
+  </ThemedButton>
 );
 
 PrimaryButton.propTypes = {
-  children: PropTypes.node.isRequired,
+  disabled: PropTypes.bool,
+  children: PropTypes.string.isRequired,
 };
