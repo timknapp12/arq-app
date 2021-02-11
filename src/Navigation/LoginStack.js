ca2133b25998326944e1632c9155703c9594e204
Tim Knapp <tknapp@qsciences.com>
2021-02-11 13:54:55 -0700

    configure login stack

--- src/Navigation/LoginStack.js
+++ src/Navigation/LoginStack.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import LoadingScreen from '../Components/LoadingScreen';
 import LoginScreen from '../Components/LoginScreen';
+import PasswordRecoveryScreen from '../Components/PasswordRecoveryScreen';
+import { white, blue } from '../Styles/colors';
 
 // source for stack navigator: https://reactnavigation.org/docs/hello-react-navigation
 const Login = createStackNavigator();
@@ -10,20 +12,35 @@ const LoginStack = () => {
   return (
     <Login.Navigator
       screenOptions={{
-        headerShown: false,
+        headerBackTitleVisible: false,
       }}>
-      <Login.Screen name="Loading Screen" component={LoadingScreen} />
       <Login.Screen
         name="Login Screen"
         component={LoginScreen}
         options={{
-          title: 'Please Login',
+          title: 'Login',
+          headerShown: false,
           gestureEnabled: false,
           headerLeft: () => {
             return null;
           },
         }}
       />
+      <Login.Screen
+        name="Password Recovery Screen"
+        component={PasswordRecoveryScreen}
+        options={{
+          title: 'Password Recovery',
+          headerStyle: { backgroundColor: blue },
+          headerTintColor: white,
+          headerTitleStyle: {
+            fontSize: 24,
+            fontFamily: 'Nunito-Regular',
+          },
+          // headerBackTitleVisible: false,
+        }}
+      />
+      <Login.Screen name="Loading Screen" component={LoadingScreen} />
     </Login.Navigator>
   );
 };
