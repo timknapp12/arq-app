6b804392f871d79dabbf8f85d3d924dff16ac994
Tim Knapp <tknapp@qsciences.com>
2021-02-11 13:57:11 -0700

    update colors and themes

---
+++ src/Styles/colors.js
@@ -0,0 +1,17 @@
+export const white = '#ffffff';
+
+export const offwhite = '#EDEDED';
+
+export const black = '#000000';
+
+export const gray = '#979797';
+
+export const lightgray = '#D8D8D8';
+
+export const darkgray = '#2C2C2C';
+
+export const blue = '#006699';
+
+export const darkblue = '#2196F3';
+
+export const red = '#FF0000';
