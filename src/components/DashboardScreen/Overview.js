38f65f47094c28d3f5258aa5847f092a50509edd
Tim Knapp <tknapp@qsciences.com>
2021-02-23 14:00:10 -0700

    add user details to overview

--- src/components/DashboardScreen/Overview.js
+++ src/components/DashboardScreen/Overview.js
@@ -1,13 +1,26 @@
 import React from 'react';
-import { H2Bold, Flexbox } from '../Common';
+import styled from 'styled-components/native';
+import { H4Bold, Flexbox, H4Secondary } from '../Common';
 import { Localized, init } from '../../Translations/Localized';
+
+const Section = styled.View``;
 
 const Overview = () => {
   init();
 
   return (
-    <Flexbox>
-      <H2Bold>{Localized('overview')}</H2Bold>
+    <Flexbox width="100%" height="100%">
+      <Flexbox padding={20} width="100%" direction="row">
+        <Section>
+          <H4Bold>Sloane Taylor</H4Bold>
+          <H4Secondary>{`${Localized('rank')}: Distributor`}</H4Secondary>
+        </Section>
+        <Section>
+          <H4Bold>2/16/20</H4Bold>
+          <H4Secondary>{Localized('join-date')}</H4Secondary>
+        </Section>
+      </Flexbox>
+      <H4Bold>{Localized('overview')}</H4Bold>
     </Flexbox>
   );
 };
