718ea7a2bf25e625676d8ba33c4f163ac32ca72e
Tim Knapp <tknapp@qsciences.com>
2021-02-24 16:24:42 -0700

    testId and a11y and i18n for overview

--- src/components/DashboardScreen/Overview.js
+++ src/components/DashboardScreen/Overview.js
@@ -4,7 +4,7 @@ import styled from 'styled-components/native';
 import { H4Bold, Flexbox, H4Secondary } from '../Common';
 import { Localized, init } from '../../Translations/Localized';
 import Donut from './Donut';
-import { pacificBlue, purple } from '../../Styles/colors';
+import { pacificBlue, darkViolet } from '../../Styles/colors';
 
 const ChartTitle = styled(H4Bold)`
   color: ${(props) => props.theme.secondaryTextColor};
@@ -15,7 +15,11 @@ const Overview = () => {
 
   return (
     <Flexbox width="100%">
-      <Flexbox padding={20} width="100%" direction="row">
+      <Flexbox
+        accessibilityLabel="Distributor name and rank"
+        padding={20}
+        width="100%"
+        direction="row">
         <View>
           <H4Bold>Sloane Taylor</H4Bold>
           <H4Secondary>{`${Localized('rank')}: Distributor`}</H4Secondary>
@@ -25,19 +29,32 @@ const Overview = () => {
           <H4Secondary>{Localized('join-date')}</H4Secondary>
         </View>
       </Flexbox>
+
       <Flexbox padding={20} width="100%" direction="row">
-        <Flexbox width="auto">
-          <ChartTitle>PV</ChartTitle>
-          <Donut percentage={175} max={175} color={pacificBlue} />
+        <Flexbox accessibilityLabel="Distributor monthly pv" width="auto">
+          <ChartTitle testID="pv-donut-label">{Localized('pv')}</ChartTitle>
+          <Donut
+            testID="pv-donut-svg"
+            percentage={175}
+            max={175}
+            color={pacificBlue}
+          />
         </Flexbox>
-        <Flexbox width="auto">
-          <ChartTitle>CV</ChartTitle>
-          <Donut percentage={256} max={256} />
+
+        <Flexbox accessibilityLabel="Distributor monthly cv" width="auto">
+          <ChartTitle testID="cv-donut-label">{Localized('cv')}</ChartTitle>
+          <Donut testID="cv-donut-svg" percentage={256} max={256} />
         </Flexbox>
       </Flexbox>
-      <Flexbox width="auto">
-        <ChartTitle>OV</ChartTitle>
-        <Donut percentage={456} max={456} color={purple} />
+
+      <Flexbox accessibilityLabel="Distributor monthly ov" width="auto">
+        <ChartTitle testID="ov-donut-label">{Localized('ov')}</ChartTitle>
+        <Donut
+          testID="ov-donut-svg"
+          percentage={456}
+          max={456}
+          color={darkViolet}
+        />
       </Flexbox>
     </Flexbox>
   );
