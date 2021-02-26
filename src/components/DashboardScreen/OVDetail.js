b04da5b53321bc40e5788404071d11affbca20b0
Tim Knapp <tknapp@qsciences.com>
2021-02-26 09:47:25 -0700

    add donuts to ov details view

--- src/components/DashboardScreen/OVDetail.js
+++ src/components/DashboardScreen/OVDetail.js
@@ -1,15 +1,89 @@
-import React from 'react';
-import { H2Bold, Flexbox } from '../Common';
+import React, { useState } from 'react';
+import Proptypes from 'prop-types';
+import styled from 'styled-components/native';
+import { H4Bold, Flexbox } from '../Common';
 import { Localized, init } from '../../Translations/Localized';
+import Slider from './Slider';
+import Donut from './Donut';
+import { pacificBlue } from '../../Styles/colors';
 
-const OVDetail = () => {
+const ChartTitle = styled(H4Bold)`
+  color: ${(props) => props.theme.secondaryTextColor};
+`;
+
+const OVDetail = ({ ranklist }) => {
   init();
+  const initialRankName = Localized('pro');
+  const [rankName, setRankName] = useState(initialRankName);
+  const initialRank = {
+    id: 2,
+    requiredPv: 100,
+    requiredQov: 600,
+    name: Localized('pro'),
+  };
+  const [rank, setRank] = useState(initialRank);
 
   return (
-    <Flexbox>
-      <H2Bold>{Localized('ov-detail')}</H2Bold>
+    <Flexbox width="100%">
+      <Slider
+        rankName={rankName}
+        setRankName={setRankName}
+        rank={rank}
+        setRank={setRank}
+        ranklist={ranklist}
+      />
+
+      <Flexbox padding={20} width="100%" direction="row">
+        <Flexbox accessibilityLabel="Distributor leg one" width="auto">
+          <ChartTitle testID="leg-one-label">{Localized('leg-one')}</ChartTitle>
+          <Donut
+            testID="leg-one-donut-svg"
+            percentage={160}
+            max={360}
+            color={pacificBlue}
+            view="ov detail"
+          />
+        </Flexbox>
+
+        <Flexbox accessibilityLabel="Distributor leg two" width="auto">
+          <ChartTitle testID="leg-two-label">{Localized('leg-two')}</ChartTitle>
+          <Donut
+            testID="leg-two-donut-svg"
+            percentage={100}
+            max={360}
+            color={pacificBlue}
+            view="ov detail"
+          />
+        </Flexbox>
+      </Flexbox>
+
+      <Flexbox accessibilityLabel="Distributor leg three" width="auto">
+        <ChartTitle testID="leg-three-label">
+          {Localized('leg-three')}
+        </ChartTitle>
+        <Donut
+          testID="leg-three-donut-svg"
+          percentage={40}
+          max={360}
+          color={pacificBlue}
+          view="ov detail"
+        />
+      </Flexbox>
     </Flexbox>
   );
+};
+
+OVDetail.propTypes = {
+  ranklist: Proptypes.arrayOf(
+    Proptypes.shape({
+      id: Proptypes.number,
+      name: Proptypes.string,
+      requiredPv: Proptypes.number,
+      requiredQov: Proptypes.number,
+      legMaxPerc: Proptypes.number,
+      legMaxOv: Proptypes.number,
+    }),
+  ),
 };
 
 export default OVDetail;
