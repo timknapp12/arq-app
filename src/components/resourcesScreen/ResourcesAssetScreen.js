9c64bad212a9ee332ca43256e6797c2d1fc687e0
Tim Knapp <tknapp@qsciences.com>
2021-04-13 12:06:08 -0600

    resource asset screen to render pdf or image

--- src/components/resourcesScreen/ResourcesAssetScreen.js
+++ src/components/resourcesScreen/ResourcesAssetScreen.js
@@ -1,12 +1,53 @@
-import React from 'react';
-import { ScreenContainer, H4 } from '../common';
+import React, { useState, useContext } from 'react';
+import PropTypes from 'prop-types';
+import { Image, Dimensions, ScrollView } from 'react-native';
+import { ScreenContainer } from '../common';
+import PDFReader from 'rn-pdf-reader-js';
+import AppContext from '../../contexts/AppContext';
 
-const ResourcesAssetScreen = () => {
-  return (
-    <ScreenContainer>
-      <H4>Asset Screen</H4>
-    </ScreenContainer>
-  );
+const ResourcesAssetScreen = ({ route }) => {
+  const { theme } = useContext(AppContext);
+  const { url, contentType } = route.params;
+  const [imageWidth, setImageWidth] = useState(300);
+  const [imageHeight, setImageHeight] = useState(150);
+
+  const { width } = Dimensions.get('window');
+  const ratio = imageWidth / imageHeight;
+  const calculatedHeight = width / ratio;
+
+  if (contentType === 'pdf') {
+    return (
+      <PDFReader
+        webviewStyle={{ backgroundColor: theme.backgroundColor }}
+        source={{ uri: url }}
+      />
+    );
+  }
+  if (contentType === 'image') {
+    Image.getSize(url, (width, height) => {
+      setImageWidth(width);
+      setImageHeight(height);
+    });
+    return (
+      <ScreenContainer>
+        <ScrollView
+          contentContainerStyle={{
+            justifyContent: 'center',
+            height: '100%',
+          }}
+          minimumZoomScale={1}
+          maximumZoomScale={5}>
+          <Image
+            resizeMode={'cover'}
+            style={{ width: width, height: calculatedHeight }}
+            source={{ uri: url }}
+          />
+        </ScrollView>
+      </ScreenContainer>
+    );
+  }
 };
-
+ResourcesAssetScreen.propTypes = {
+  route: PropTypes.object,
+};
 export default ResourcesAssetScreen;
