dec4cb87c963d75d3cce2771c16383e634c518d8
Tim Knapp <tknapp@qsciences.com>
2021-04-26 09:44:15 -0600

    add translations to download file toast

--- src/utils/downloadFile.js
+++ src/utils/downloadFile.js
@@ -2,6 +2,7 @@ import * as FileSystem from 'expo-file-system';
 import * as Permissions from 'expo-permissions';
 import * as MediaLibrary from 'expo-media-library';
 import * as Sharing from 'expo-sharing';
+import { Localized, initLanguage } from '../translations/Localized';
 
 // if the file is an image then the app needs permission to access the photos on the device
 const getMediaLibraryPermissions = async () => {
@@ -14,6 +15,7 @@ export const downloadFile = async (
   contentType,
   setToastInfo,
 ) => {
+  initLanguage();
   // The callback function runs as the download is in progress
   let progress;
   const callback = (downloadProgress) => {
@@ -24,9 +26,14 @@ export const downloadFile = async (
     );
     // this block opens the toast to notify user that the download process has begun and current percent finished
     if (contentType === 'image') {
-      setToastInfo(filename, `Downloading to Photos...`, true, progress);
+      setToastInfo(
+        filename,
+        Localized('Downloading to Photos...'),
+        true,
+        progress,
+      );
     } else {
-      setToastInfo(filename, `Downloading...`, true, progress);
+      setToastInfo(filename, Localized('Downloading...'), true, progress);
     }
     return progress;
   };
@@ -45,7 +52,7 @@ export const downloadFile = async (
   // this block updates the toast to notify user that download is complete
   if (contentType === 'image') {
     setTimeout(() => {
-      setToastInfo(filename, `Saved to Photos`, false);
+      setToastInfo(filename, Localized('Saved to Photos'), false);
     }, 1000);
     // if the file is an image then it will be saved directly into the photos on the device
     try {
@@ -56,7 +63,7 @@ export const downloadFile = async (
     // if the file is NOT an image then the user will select where to save the file on the device
   } else {
     setTimeout(() => {
-      setToastInfo(filename, `Download Complete!`, false);
+      setToastInfo(filename, Localized('Download Complete'), false);
     }, 1000);
     try {
       Sharing.shareAsync(localUrl);
