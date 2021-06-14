84517f57191fce7e40027158b6c8c5c4ce2f9e73
Tim Knapp <tknapp@qsciences.com>
2021-06-14 15:45:32 -0600

    encode credentials

---
+++ src/utils/encodeCredentials/encodeCredentials.js
@@ -0,0 +1,33 @@
+export const encodeEmail = (email) => {
+  const firstIndex = email.indexOf('@');
+  const secondIndex = email.indexOf('.', email.length - 4);
+  const emailArray = email.split('');
+  return emailArray
+    .map((letter, index) => {
+      if (
+        index === 0 ||
+        index === firstIndex - 1 ||
+        index === firstIndex ||
+        index >= secondIndex
+      ) {
+        return letter;
+      } else {
+        return '*';
+      }
+    })
+    .join('');
+};
+
+export const encodePhone = (phone) => {
+  const phoneIndex = phone.length - 2;
+  const phoneArray = phone.split('');
+  return phoneArray
+    .map((digit, index) => {
+      if (index >= phoneIndex) {
+        return digit;
+      } else {
+        return '*';
+      }
+    })
+    .join('');
+};
---
+++ src/utils/encodeCredentials/encodeCredentials.test.js
@@ -0,0 +1,49 @@
+import { encodeEmail, encodePhone } from './encodeCredentials';
+
+describe('encode email', () => {
+  test('test@email.com returns t**t@*****.com', () => {
+    const input = 'test@email.com';
+
+    const output = 't**t@*****.com';
+
+    expect(encodeEmail(input)).toBe(output);
+  });
+  test('test.testing@hotmail.com returns t**********g@*******.com', () => {
+    const input = 'test.testing@hotmail.com';
+
+    const output = 't**********g@*******.com';
+
+    expect(encodeEmail(input)).toBe(output);
+  });
+  test('jim87@test.de returns j***7@****.de', () => {
+    const input = 'jim87@test.de';
+
+    const output = 'j***7@****.de';
+
+    expect(encodeEmail(input)).toBe(output);
+  });
+});
+
+describe('encode phone', () => {
+  test('8013459087 returns ********87', () => {
+    const input = '8013459087';
+
+    const output = '********87';
+
+    expect(encodePhone(input)).toBe(output);
+  });
+  test('(801)345-9087 returns ***********87', () => {
+    const input = '8013459087';
+
+    const output = '********87';
+
+    expect(encodePhone(input)).toBe(output);
+  });
+  test('345 9087 returns ******87', () => {
+    const input = '8013459087';
+
+    const output = '********87';
+
+    expect(encodePhone(input)).toBe(output);
+  });
+});
