bdcf72d0dfb6110b62397d94b34c9793656cc966
Tim Knapp <tknapp@qsciences.com>
2021-07-19 10:07:13 -0600

    don't find marketUrl unless markets exist

--- src/utils/markets/findMarketId.js
+++ src/utils/markets/findMarketId.js
@@ -1,4 +1,4 @@
-export const findMarketId = (marketCode, array = []) => {
+export const findMarketId = (marketCode, array) => {
   const result = array.find((item) => item.countryCode === marketCode);
   return result.countryId;
 };
