cca59ff64baef40b867705e8947c08da9b9a18e6
Tim Knapp <tknapp@qsciences.com>
2021-08-04 15:15:19 -0600

    calculate total unread notifications

---
+++ src/utils/notifications/mockNotifications.js
@@ -0,0 +1,140 @@
+export const totalArray = [
+  {
+    viewId: 34,
+    isSaved: false,
+    isReadByAssociate: false,
+    dateViewUtc: '2021-08-04T19:49:36.361Z',
+    prospect: {
+      firstName: 'Lewis',
+      lastName: 'Bass',
+      prospectId: '4200cf70-3e5e-4ae1-8178-e912d6be3cf2',
+    },
+    sentLinks: {
+      displayName: 'Lewis Bass',
+      sentLinkId: 'ffd48217-3cac-432a-be8f-f78f1b18a691',
+    },
+  },
+  {
+    viewId: 33,
+    isSaved: false,
+    isReadByAssociate: false,
+    dateViewUtc: '2021-08-04T19:49:22.563Z',
+    prospect: {
+      firstName: 'Lewis',
+      lastName: 'Bass',
+      prospectId: '4200cf70-3e5e-4ae1-8178-e912d6be3cf2',
+    },
+    sentLinks: {
+      displayName: 'Lewis Bass',
+      sentLinkId: 'ffd48217-3cac-432a-be8f-f78f1b18a691',
+    },
+  },
+  {
+    viewId: 32,
+    isSaved: false,
+    isReadByAssociate: false,
+    dateViewUtc: '2021-08-04T19:49:09.864Z',
+    prospect: {
+      firstName: 'Lewis',
+      lastName: 'Bass',
+      prospectId: '4200cf70-3e5e-4ae1-8178-e912d6be3cf2',
+    },
+    sentLinks: {
+      displayName: 'Lewis Bass',
+      sentLinkId: 'ffd48217-3cac-432a-be8f-f78f1b18a691',
+    },
+  },
+  {
+    viewId: 31,
+    isSaved: false,
+    isReadByAssociate: false,
+    dateViewUtc: '2021-08-04T19:48:32.585Z',
+    prospect: {
+      firstName: 'John',
+      lastName: 'Douglas',
+      prospectId: 'a8c0577a-9dca-4e74-ac10-87a142b7d143',
+    },
+    sentLinks: {
+      displayName: 'John Douglas',
+      sentLinkId: '1cde5580-6a13-4184-ba5b-58c2f69acfc0',
+    },
+  },
+  {
+    viewId: 30,
+    isSaved: false,
+    isReadByAssociate: false,
+    dateViewUtc: '2021-08-04T19:46:44.965Z',
+    prospect: {
+      firstName: 'Lewis',
+      lastName: 'Bass',
+      prospectId: '4200cf70-3e5e-4ae1-8178-e912d6be3cf2',
+    },
+    sentLinks: {
+      displayName: 'Lewis Bass',
+      sentLinkId: '9f485307-c1b6-4e34-ac94-94dc276a9d32',
+    },
+  },
+  {
+    viewId: 29,
+    isSaved: false,
+    isReadByAssociate: false,
+    dateViewUtc: '2021-08-04T19:46:31.601Z',
+    prospect: {
+      firstName: 'Lewis',
+      lastName: 'Bass',
+      prospectId: '4200cf70-3e5e-4ae1-8178-e912d6be3cf2',
+    },
+    sentLinks: {
+      displayName: 'Lewis Bass',
+      sentLinkId: '9f485307-c1b6-4e34-ac94-94dc276a9d32',
+    },
+  },
+];
+
+export const shortArray = [
+  {
+    viewId: 31,
+    isSaved: false,
+    isReadByAssociate: false,
+    dateViewUtc: '2021-08-04T19:48:32.585Z',
+    prospect: {
+      firstName: 'John',
+      lastName: 'Douglas',
+      prospectId: 'a8c0577a-9dca-4e74-ac10-87a142b7d143',
+    },
+    sentLinks: {
+      displayName: 'John Douglas',
+      sentLinkId: '1cde5580-6a13-4184-ba5b-58c2f69acfc0',
+    },
+  },
+  {
+    viewId: 30,
+    isSaved: false,
+    isReadByAssociate: false,
+    dateViewUtc: '2021-08-04T19:46:44.965Z',
+    prospect: {
+      firstName: 'Lewis',
+      lastName: 'Bass',
+      prospectId: '4200cf70-3e5e-4ae1-8178-e912d6be3cf2',
+    },
+    sentLinks: {
+      displayName: 'Lewis Bass',
+      sentLinkId: '9f485307-c1b6-4e34-ac94-94dc276a9d32',
+    },
+  },
+  {
+    viewId: 29,
+    isSaved: false,
+    isReadByAssociate: false,
+    dateViewUtc: '2021-08-04T19:46:31.601Z',
+    prospect: {
+      firstName: 'Lewis',
+      lastName: 'Bass',
+      prospectId: '4200cf70-3e5e-4ae1-8178-e912d6be3cf2',
+    },
+    sentLinks: {
+      displayName: 'Lewis Bass',
+      sentLinkId: '9f485307-c1b6-4e34-ac94-94dc276a9d32',
+    },
+  },
+];
