a677b085f0f18e4212c83f330f83daa9928b2aa0
Tim Knapp <tknapp@qsciences.com>
2021-09-27 10:07:06 -0600

    add team stack

---
+++ src/components/teamScreen/TeamScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const TeamScreen = () => {
+  return (
+    <View>
+      <Text>Team Screen</Text>
+    </View>
+  );
+};
+
+export default TeamScreen;
