46807dce0d6b1d7d84a51f13b4d7f4394e854476
Tim Knapp <tknapp@qsciences.com>
2021-10-28 12:20:10 -0600

    tests for finding members in downline

--- src/utils/teamView/filterDownline.test.js
+++ src/utils/teamView/filterDownline.test.js
@@ -1,8 +1,8 @@
-import { findActiveAmbassadors } from './filterDownline';
+import { findMembersInDownlineOneLevel } from './filterDownline';
 import { mockChildTree } from './mockDownlineData';
 
-describe('findActiveAmbassadors', () => {
-  test('returns type of AMBASSADOR only that are NOT TERMINATED', () => {
+describe('findMembersInDownlineOneLevel', () => {
+  test('returns type of AMBASSADOR', () => {
     const output = [
       {
         associate: {
@@ -60,6 +60,98 @@ describe('findActiveAmbassadors', () => {
       },
     ];
 
-    expect(findActiveAmbassadors(mockChildTree)).toStrictEqual(output);
+    expect(
+      findMembersInDownlineOneLevel(mockChildTree, 'AMBASSADOR'),
+    ).toStrictEqual(output);
+  });
+
+  test('returns type of PREFERRED', () => {
+    const output = [
+      {
+        associate: {
+          associateId: 85981,
+          legacyAssociateId: 319342,
+          firstName: 'Kelsey',
+          lastName: 'Bowman',
+          profileUrl: null,
+          associateType: 'PREFERRED',
+          associateStatus: 'TERMINATED',
+        },
+        rank: {
+          rankId: 1,
+          rankName: 'Ambassador',
+        },
+        pv: 0,
+        qoV: 0,
+        pa: 0,
+      },
+      {
+        associate: {
+          associateId: 86478,
+          legacyAssociateId: 321223,
+          firstName: 'Brooke',
+          lastName: 'Mutapcic',
+          profileUrl: null,
+          associateType: 'PREFERRED',
+          associateStatus: 'TERMINATED',
+        },
+        rank: {
+          rankId: 1,
+          rankName: 'Ambassador',
+        },
+        pv: 0,
+        qoV: 0,
+        pa: 0,
+      },
+      {
+        associate: {
+          associateId: 93133,
+          legacyAssociateId: 353092,
+          firstName: 'Michelle',
+          lastName: 'Bartusek',
+          profileUrl: null,
+          associateType: 'PREFERRED',
+          associateStatus: 'TERMINATED',
+        },
+        rank: {
+          rankId: 1,
+          rankName: 'Ambassador',
+        },
+        pv: 0,
+        qoV: 0,
+        pa: 0,
+      },
+    ];
+
+    expect(
+      findMembersInDownlineOneLevel(mockChildTree, 'PREFERRED'),
+    ).toStrictEqual(output);
+  });
+
+  test('returns type of RETAIL', () => {
+    const output = [
+      {
+        associate: {
+          associateId: 86111,
+          legacyAssociateId: 319822,
+          firstName: 'Misty',
+          lastName: 'Rivera',
+          profileUrl: null,
+          associateType: 'RETAIL',
+          associateStatus: 'TERMINATED',
+        },
+        rank: {
+          rankId: 1,
+          rankName: 'Ambassador',
+        },
+        pv: 0,
+        qoV: 0,
+        pa: 0,
+      },
+    ];
+
+    expect(
+      findMembersInDownlineOneLevel(mockChildTree, 'RETAIL'),
+    ).toStrictEqual(output);
   });
 });
