f57707f45275cfd3a772fb812de41fcf107fbe90
Tim Knapp <tknapp@qsciences.com>
2021-11-17 11:13:24 -0700

    rebase tranlsations

--- src/components/teamScreen/myTeam/SearchDownlineScreen.js
+++ src/components/teamScreen/myTeam/SearchDownlineScreen.js
@@ -1,11 +1,14 @@
-import React, { useState } from 'react';
-import { FlatList } from 'react-native';
+import React, { useState, useContext } from 'react';
+import { FlatList, View } from 'react-native';
 import { ScreenContainer, Flexbox, Input } from '../../common';
+import FilterIcon from '../../../../assets/icons/filter-icon.svg';
 import DownlineProfileInfoContainer from './DownlineProfileInfoContainer';
 import { searchResults } from './mockSearchResults';
+import AppContext from '../../../contexts/AppContext';
 import { CardContainer } from './myTeamCard.styles';
 
 const SearchDownlineScreen = () => {
+  const { theme } = useContext(AppContext);
   const [searchTerm, setSearchTerm] = useState('');
 
   const renderItem = ({ item }) => (
@@ -23,19 +26,35 @@ const SearchDownlineScreen = () => {
         height: '100%',
       }}
     >
-      <Flexbox width="100%" style={{ flex: 1 }}>
-        <Flexbox width="85%" style={{ marginTop: 18 }} padding={4}>
-          <Input
-            autoFocus
-            testID="propsect-search-input"
-            value={searchTerm}
-            onChangeText={(text) => setSearchTerm(text)}
-            returnKeyType="done"
+      <Flexbox width="100%" height="100%">
+        <Flexbox direction="row" justify="space-between" padding={8}>
+          <FilterIcon
+            style={{
+              height: 30,
+              width: 30,
+              color: theme.primaryTextColor,
+            }}
+          />
+          <Flexbox width="75%">
+            <Input
+              autoFocus
+              testID="propsect-search-input"
+              value={searchTerm}
+              onChangeText={(text) => setSearchTerm(text)}
+              returnKeyType="done"
+              placeholder="Search by first and last name"
+              placeholderTextColor={theme.placeholderTextColor}
+            />
+          </Flexbox>
+          <View
+            style={{
+              width: 10,
+            }}
           />
         </Flexbox>
-        <Flexbox width="95%" style={{ marginTop: 10 }}>
+        <Flexbox width="95%" style={{ marginTop: 12 }}>
           <FlatList
-            style={{ width: '100%', marginBottom: 60 }}
+            style={{ width: '100%', marginBottom: 50 }}
             data={searchResults}
             renderItem={renderItem}
             keyExtractor={(item) => item?.associate?.associateId?.toString()}
