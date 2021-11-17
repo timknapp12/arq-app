f8ca66e24cf516cf9345bb4c3256678f5d4b41e7
Tim Knapp <tknapp@qsciences.com>
2021-11-17 10:55:36 -0700

    add mock data

---
+++ src/components/teamScreen/myTeam/mockSearchResults.js
@@ -0,0 +1,191 @@
+export const searchResults = [
+  {
+    uplineTreeNode: {
+      associate: {
+        associateId: 74951,
+        legacyAssociateId: 271471,
+      },
+    },
+    associate: {
+      associateId: 75251,
+      legacyAssociateId: 272614,
+      firstName: 'Danielle',
+      lastName: 'Glymph',
+      profileUrl: null,
+      associateType: 'AMBASSADOR',
+      associateStatus: 'ACTIVE',
+    },
+    rank: {
+      rankId: 3,
+      rankName: 'Builder',
+    },
+  },
+  {
+    uplineTreeNode: {
+      associate: {
+        associateId: 74691,
+        legacyAssociateId: 270412,
+      },
+    },
+    associate: {
+      associateId: 74951,
+      legacyAssociateId: 271471,
+      firstName: 'Dustin',
+      lastName: 'Free',
+      profileUrl: null,
+      associateType: 'PREFERRED',
+      associateStatus: 'ACTIVE',
+    },
+    rank: {
+      rankId: 1,
+      rankName: 'Ambassador',
+    },
+  },
+  {
+    uplineTreeNode: {
+      associate: {
+        associateId: 74951,
+        legacyAssociateId: 271471,
+      },
+    },
+    associate: {
+      associateId: 75400,
+      legacyAssociateId: 273181,
+      firstName: 'Mary Anne',
+      lastName: 'Lewis',
+      profileUrl: null,
+      associateType: 'RETAIL',
+      associateStatus: 'ACTIVE',
+    },
+    rank: {
+      rankId: 1,
+      rankName: 'Ambassador',
+    },
+  },
+  {
+    uplineTreeNode: {
+      associate: {
+        associateId: 74691,
+        legacyAssociateId: 270412,
+      },
+    },
+    associate: {
+      associateId: 75101,
+      legacyAssociateId: 272008,
+      firstName: 'DENISE',
+      lastName: 'POTTROFF',
+      profileUrl: null,
+      associateType: 'PREFERRED',
+      associateStatus: 'TERMINATED',
+    },
+    rank: {
+      rankId: 1,
+      rankName: 'Ambassador',
+    },
+  },
+  {
+    uplineTreeNode: {
+      associate: {
+        associateId: 74691,
+        legacyAssociateId: 270412,
+      },
+    },
+    associate: {
+      associateId: 75197,
+      legacyAssociateId: 272398,
+      firstName: 'Karlie',
+      lastName: 'Hodgen',
+      profileUrl: null,
+      associateType: 'AMBASSADOR',
+      associateStatus: 'FRAUDULENT_BEHAVIOR',
+    },
+    rank: {
+      rankId: 1,
+      rankName: 'Ambassador',
+    },
+  },
+  {
+    uplineTreeNode: {
+      associate: {
+        associateId: 75197,
+        legacyAssociateId: 272398,
+      },
+    },
+    associate: {
+      associateId: 75992,
+      legacyAssociateId: 275641,
+      firstName: 'Tim',
+      lastName: 'Foster',
+      profileUrl: null,
+      associateType: 'PREFERRED',
+      associateStatus: 'SUSPENDED',
+    },
+    rank: {
+      rankId: 1,
+      rankName: 'Ambassador',
+    },
+  },
+  {
+    uplineTreeNode: {
+      associate: {
+        associateId: 75197,
+        legacyAssociateId: 272398,
+      },
+    },
+    associate: {
+      associateId: 81016,
+      legacyAssociateId: 298012,
+      firstName: 'Hateesha',
+      lastName: 'Brown',
+      profileUrl: null,
+      associateType: 'AMBASSADOR',
+      associateStatus: 'CHARGEBACK',
+    },
+    rank: {
+      rankId: 1,
+      rankName: 'Ambassador',
+    },
+  },
+  {
+    uplineTreeNode: {
+      associate: {
+        associateId: 75197,
+        legacyAssociateId: 272398,
+      },
+    },
+    associate: {
+      associateId: 83203,
+      legacyAssociateId: 307258,
+      firstName: 'Cheryl',
+      lastName: 'Pelto',
+      profileUrl: null,
+      associateType: 'AMBASSADOR',
+      associateStatus: 'DOCU_SIGN_HOLD',
+    },
+    rank: {
+      rankId: 1,
+      rankName: 'Ambassador',
+    },
+  },
+  {
+    uplineTreeNode: {
+      associate: {
+        associateId: 75197,
+        legacyAssociateId: 272398,
+      },
+    },
+    associate: {
+      associateId: 86179,
+      legacyAssociateId: 320095,
+      firstName: 'Jeani K',
+      lastName: 'Hodgen',
+      profileUrl: null,
+      associateType: 'PREFERRED',
+      associateStatus: 'TERMINATED',
+    },
+    rank: {
+      rankId: 1,
+      rankName: 'Ambassador',
+    },
+  },
+];
