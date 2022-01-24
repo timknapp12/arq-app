2294e3a3976e79c20d17e071ee6b042bfa3f9124
Tim Knapp <tknapp@qsciences.com>
2022-01-24 16:54:38 -0700

    add collapsable input

--- src/components/teamScreen/visualTree/VisualTreeSearchBar.js
+++ src/components/teamScreen/visualTree/VisualTreeSearchBar.js
@@ -1,36 +1,46 @@
-import React, { useContext } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { TouchableOpacity } from 'react-native';
 import { Flexbox } from '../../common';
-import SearchIcon from '../../../../assets/icons/search-icon.svg';
-import AppContext from '../../../contexts/AppContext';
+// import SearchIcon from '../../../../assets/icons/search-icon.svg';
 import { RoundButton } from './visualTree.styles';
+import FilterSearchBar from '../../filterSearchBar/FilterSearchBar';
+import CollapsableInput from './CollapsableInput';
 
-const VisualTreeSearchBar = ({ selectedPane, setSelectedPane }) => {
-  const { theme } = useContext(AppContext);
+const VisualTreeSearchBar = ({
+  selectedPane,
+  setSelectedPane,
+  searchTerm,
+  setSearchTerm,
+}) => {
+  const [isSearchInputExpanded, setIsSearchInputExpanded] = useState(false);
 
-  const onPress = () => {};
   return (
-    <Flexbox direction="row">
-      <Flexbox direction="row" width="100px">
-        <RoundButton
-          selected={selectedPane === 1}
-          onPress={() => setSelectedPane(1)}
-        />
-        <RoundButton
-          selected={selectedPane === 2}
-          onPress={() => setSelectedPane(2)}
-        />
-        <RoundButton
-          selected={selectedPane === 3}
-          onPress={() => setSelectedPane(3)}
+    <Flexbox>
+      <FilterSearchBar
+        onPress={() => setIsSearchInputExpanded((state) => !state)}
+      >
+        <Flexbox direction="row" width="100px">
+          <RoundButton
+            selected={selectedPane === 1}
+            onPress={() => setSelectedPane(1)}
+          />
+          <RoundButton
+            selected={selectedPane === 2}
+            onPress={() => setSelectedPane(2)}
+          />
+          <RoundButton
+            selected={selectedPane === 3}
+            onPress={() => setSelectedPane(3)}
+          />
+        </Flexbox>
+      </FilterSearchBar>
+      <Flexbox style={{ paddingEnd: 12, paddingStart: 12 }}>
+        <CollapsableInput
+          isOpen={isSearchInputExpanded}
+          value={searchTerm}
+          onChangeText={setSearchTerm}
         />
       </Flexbox>
-      <TouchableOpacity onPress={onPress}>
-        <SearchIcon
-          style={{ height: 36, width: 36, color: theme.primaryTextColor }}
-        />
-      </TouchableOpacity>
     </Flexbox>
   );
 };
@@ -38,6 +48,8 @@ const VisualTreeSearchBar = ({ selectedPane, setSelectedPane }) => {
 VisualTreeSearchBar.propTypes = {
   selectedPane: PropTypes.number.isRequired,
   setSelectedPane: PropTypes.func.isRequired,
+  searchTerm: PropTypes.string.isRequired,
+  setSearchTerm: PropTypes.func.isRequired,
 };
 
 export default VisualTreeSearchBar;
--- src/components/teamScreen/visualTree/VisualTreeView.js
+++ src/components/teamScreen/visualTree/VisualTreeView.js
@@ -1,183 +1,33 @@
-import React, { useState, useContext } from 'react';
+import React, { useState } from 'react';
+import { ScrollView } from 'react-native';
 import { Flexbox } from '../../common';
-import mockTreeData from './mockTreeData';
-import reformatListForVisualTreeBubbles from '../../../utils/teamView/reformatListForVisualTreeBubbles';
 import VisualTreeSearchBar from './VisualTreeSearchBar';
-import { DraxProvider } from 'react-native-drax';
-import VisualTreeBubble from './VisualTreeBubble';
-import AppContext from '../../../contexts/AppContext';
-import { OuterCircle, ReceivingCircle } from './visualTree.styles';
+import VisualTreePane from './VisualTreePane';
 
 // source for finding coordinates https://stackoverflow.com/questions/26599782/positioning-divs-in-a-circle-using-javascript
 //   (x, y) = (rx * cos(θ), ry * sin(θ)) to find coordinates on a circle
 
 // source for drag n drop library https://github.com/nuclearpasta/react-native-drax#usage
 
-const paddingOffset = 50;
-const radius = 150 - paddingOffset;
-
 const VisibilityTreeView = () => {
-  const { theme } = useContext(AppContext);
-
+  const [searchTerm, setSearchTerm] = useState('');
   const [selectedPane, setSelectedPane] = useState(1);
-  const [receiveCirlceBorderColor, setReceiveCirlceBorderColor] = useState(
-    theme.disabledTextColor,
-  );
-  const [outerCircleReceiveBorderColor, setOuterCircleReceiveBorderColor] =
-    useState(theme.disabledTextColor);
-  const [idOfDraggedItem, setIdOfDraggedItem] = useState(null);
-  console.log('idOfDraggedItem', idOfDraggedItem);
-  const uplineId = -1;
-
-  const outerCircleDiameter = 320;
-
-  const treeListCopy = [...mockTreeData];
-
-  const [outsideList, insideItem] =
-    reformatListForVisualTreeBubbles(treeListCopy);
-
-  const onDragStart = (item) => {
-    setReceiveCirlceBorderColor(theme.primaryButtonBackgroundColor);
-    setIdOfDraggedItem(item.id);
-  };
-
-  const onDragEnd = () => setReceiveCirlceBorderColor(theme.disabledTextColor);
-
-  const onDragDrop = () => setReceiveCirlceBorderColor(theme.disabledTextColor);
-
-  const onDragStartUpline = (item) => {
-    setOuterCircleReceiveBorderColor(theme.primaryButtonBackgroundColor);
-    setIdOfDraggedItem(item.id);
-  };
-
-  const onDragEndUpline = () =>
-    setOuterCircleReceiveBorderColor(theme.disabledTextColor);
-
-  const onDragDropUpline = () =>
-    setOuterCircleReceiveBorderColor(theme.disabledTextColor);
 
   return (
     <Flexbox
       justify="flex-start"
       width="95%"
-      height="100%"
+      height="120%"
       padding={4}
       style={{ zIndex: -1, maxWidth: 425 }}
     >
       <VisualTreeSearchBar
         selectedPane={selectedPane}
         setSelectedPane={setSelectedPane}
+        searchTerm={searchTerm}
+        setSearchTerm={setSearchTerm}
       />
-      <DraxProvider>
-        <Flexbox>
-          <ReceivingCircle
-            borderColor={receiveCirlceBorderColor}
-            receivingStyle={
-              uplineId !== idOfDraggedItem && { backgroundColor: 'green' }
-            }
-            onReceiveDragEnter={({ dragged: { payload } }) => {
-              console.log(`Entered ${payload}`);
-            }}
-            onReceiveDragExit={({ dragged: { payload } }) => {
-              console.log(`LEAVING ${payload}`);
-            }}
-            onReceiveDragDrop={({ dragged: { payload } }) => {
-              console.log(`REVEIVED ${payload}`);
-            }}
-          >
-            <VisualTreeBubble
-              style={{ position: 'absolute', top: -9, left: 3 }}
-              item={{ id: -1, firstName: 'Upline', lastName: 'Smithson' }}
-              draggable={true}
-              onDragStart={() => onDragStartUpline({ id: -1 })}
-              onDragEnd={onDragEndUpline}
-              onDragDrop={onDragDropUpline}
-              payload={-1}
-            />
-          </ReceivingCircle>
-          <OuterCircle
-            borderColor={outerCircleReceiveBorderColor}
-            receivingStyle={
-              uplineId === idOfDraggedItem && { backgroundColor: 'green' }
-            }
-            style={{
-              width: outerCircleDiameter,
-              height: outerCircleDiameter,
-              borderRadius: outerCircleDiameter / 2,
-            }}
-            onReceiveDragEnter={({ dragged: { payload } }) => {
-              console.log(`Entered ${payload}`);
-            }}
-            onReceiveDragExit={({ dragged: { payload } }) => {
-              console.log(`LEAVING ${payload}`);
-            }}
-            onReceiveDragDrop={({ dragged: { payload } }) => {
-              console.log(`REVEIVED ${payload}`);
-            }}
-          >
-            {outsideList?.map((item, index) => (
-              <VisualTreeBubble
-                key={item.id}
-                item={item}
-                draggable={true}
-                longPressDelay={200}
-                onPress={() => console.log('on Press')}
-                onLongPress={() => console.log('on Long Press')}
-                onDragStart={() => onDragStart(item)}
-                onDragEnd={onDragEnd}
-                onDragDrop={onDragDrop}
-                payload="world"
-                style={{
-                  top:
-                    radius -
-                    radius *
-                      Math.cos(
-                        (360 / outsideList.length) * ((index * Math.PI) / 180),
-                      ),
-                  left:
-                    radius +
-                    radius *
-                      Math.sin(
-                        (360 / outsideList.length) * ((index * Math.PI) / 180),
-                      ),
-                }}
-              />
-            ))}
-            {insideItem !== null && (
-              <VisualTreeBubble
-                onPress={() => console.log('on Press')}
-                onLongPress={() => console.log('on Long Press')}
-                item={insideItem}
-                draggable={true}
-                onDragStart={() => onDragStart(insideItem)}
-                onDragEnd={onDragEnd}
-                onDragDrop={onDragDrop}
-                payload="middle bubble"
-                style={{
-                  position: 'absolute',
-                  top: radius - 0,
-                  left: radius + 12,
-                }}
-              />
-            )}
-          </OuterCircle>
-          <ReceivingCircle
-            borderColor={receiveCirlceBorderColor}
-            receivingStyle={
-              uplineId !== idOfDraggedItem && { backgroundColor: 'green' }
-            }
-            onReceiveDragEnter={({ dragged: { payload } }) => {
-              console.log(`Entered ${payload}`);
-            }}
-            onReceiveDragExit={({ dragged: { payload } }) => {
-              console.log(`LEAVING ${payload}`);
-            }}
-            onReceiveDragDrop={({ dragged: { payload } }) => {
-              console.log(`RECEIVED ${payload}`);
-            }}
-          />
-        </Flexbox>
-      </DraxProvider>
+      <ScrollView>{selectedPane === 1 && <VisualTreePane />}</ScrollView>
     </Flexbox>
   );
 };
