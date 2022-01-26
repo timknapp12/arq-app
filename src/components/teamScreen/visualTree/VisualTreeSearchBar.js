6a92788c70f83a93e9db5934365f5afe4a3f53de
Tim Knapp <tknapp@qsciences.com>
2022-01-26 09:42:39 -0700

    add screen to search visual tree

--- src/components/teamScreen/visualTree/VisualTreeSearchBar.js
+++ src/components/teamScreen/visualTree/VisualTreeSearchBar.js
@@ -1,47 +1,46 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
+import { useNavigation } from '@react-navigation/native';
 import { Flexbox } from '../../common';
-// import SearchIcon from '../../../../assets/icons/search-icon.svg';
 import { RoundButton } from './visualTree.styles';
 import FilterSearchBar from '../../filterSearchBar/FilterSearchBar';
-import CollapsableInput from './CollapsableInput';
+import { Localized } from '../../../translations/Localized';
 
 const VisualTreeSearchBar = ({
   selectedPane,
   setSelectedPane,
-  searchTerm,
-  setSearchTerm,
+  paneOneId,
+  paneTwoId,
+  paneThreeId,
 }) => {
-  const [isSearchInputExpanded, setIsSearchInputExpanded] = useState(false);
+  const navigation = useNavigation();
+  const navigateToSearchScreen = () => {
+    navigation.navigate('Search Visual Tree Screen', {
+      title: Localized('Search My Team'),
+      selectedPane,
+      paneOneId,
+      paneTwoId,
+      paneThreeId,
+    });
+  };
 
   return (
-    <Flexbox>
-      <FilterSearchBar
-        onPress={() => setIsSearchInputExpanded((state) => !state)}
-      >
-        <Flexbox direction="row" width="100px">
-          <RoundButton
-            selected={selectedPane === 1}
-            onPress={() => setSelectedPane(1)}
-          />
-          <RoundButton
-            selected={selectedPane === 2}
-            onPress={() => setSelectedPane(2)}
-          />
-          <RoundButton
-            selected={selectedPane === 3}
-            onPress={() => setSelectedPane(3)}
-          />
-        </Flexbox>
-      </FilterSearchBar>
-      <Flexbox style={{ paddingEnd: 12, paddingStart: 12 }}>
-        <CollapsableInput
-          isOpen={isSearchInputExpanded}
-          value={searchTerm}
-          onChangeText={setSearchTerm}
+    <FilterSearchBar onPress={navigateToSearchScreen}>
+      <Flexbox direction="row" width="100px">
+        <RoundButton
+          selected={selectedPane === 1}
+          onPress={() => setSelectedPane(1)}
+        />
+        <RoundButton
+          selected={selectedPane === 2}
+          onPress={() => setSelectedPane(2)}
+        />
+        <RoundButton
+          selected={selectedPane === 3}
+          onPress={() => setSelectedPane(3)}
         />
       </Flexbox>
-    </Flexbox>
+    </FilterSearchBar>
   );
 };
 
@@ -50,6 +49,9 @@ VisualTreeSearchBar.propTypes = {
   setSelectedPane: PropTypes.func.isRequired,
   searchTerm: PropTypes.string.isRequired,
   setSearchTerm: PropTypes.func.isRequired,
+  paneOneId: PropTypes.number.isRequired,
+  paneTwoId: PropTypes.number.isRequired,
+  paneThreeId: PropTypes.number.isRequired,
 };
 
 export default VisualTreeSearchBar;
--- src/components/teamScreen/visualTree/VisualTreeView.js
+++ src/components/teamScreen/visualTree/VisualTreeView.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import { ScrollView } from 'react-native';
-import { Flexbox } from '../../common';
+import PropTypes from 'prop-types';
+import { Flexbox, MainScrollView } from '../../common';
 import VisualTreeSearchBar from './VisualTreeSearchBar';
 import VisualTreePane from './VisualTreePane';
 
@@ -9,15 +9,19 @@ import VisualTreePane from './VisualTreePane';
 
 // source for drag n drop library https://github.com/nuclearpasta/react-native-drax#usage
 
-const VisibilityTreeView = () => {
+const VisibilityTreeView = ({ paneOneId, paneTwoId, paneThreeId }) => {
   const [searchTerm, setSearchTerm] = useState('');
   const [selectedPane, setSelectedPane] = useState(1);
+
+  console.log('paneOneId', paneOneId);
+  console.log('paneTwoId', paneTwoId);
+  console.log('paneThreeId', paneThreeId);
 
   return (
     <Flexbox
       justify="flex-start"
-      width="95%"
-      height="120%"
+      width="100%"
+      height="100%"
       padding={4}
       style={{ zIndex: -1, maxWidth: 425 }}
     >
@@ -26,10 +30,21 @@ const VisibilityTreeView = () => {
         setSelectedPane={setSelectedPane}
         searchTerm={searchTerm}
         setSearchTerm={setSearchTerm}
+        paneOneId={paneOneId}
+        paneTwoId={paneTwoId}
+        paneThreeId={paneThreeId}
       />
-      <ScrollView>{selectedPane === 1 && <VisualTreePane />}</ScrollView>
+      <MainScrollView>
+        {selectedPane === 1 && <VisualTreePane />}
+      </MainScrollView>
     </Flexbox>
   );
+};
+
+VisibilityTreeView.propTypes = {
+  paneOneId: PropTypes.number,
+  paneTwoId: PropTypes.number,
+  paneThreeId: PropTypes.number,
 };
 
 export default VisibilityTreeView;
