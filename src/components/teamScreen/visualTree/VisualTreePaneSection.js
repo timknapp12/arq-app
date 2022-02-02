4f6d0b084b0be3ec31e23fe5b4562e7eb85016c0
Tim Knapp <tknapp@qsciences.com>
2022-02-02 12:47:55 -0700

    add styling to visual bubbles that are dragged

---
+++ src/components/teamScreen/visualTree/VisualTreePaneSection.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const VisualTreePaneSection = () => {
+  return (
+    <View>
+      <Text>Visual Tree pane section</Text>
+    </View>
+  );
+};
+
+export default VisualTreePaneSection;
