d17c56f69110da5dee7f3c3280d5a06a4340d25e
Tim Knapp <tknapp@qsciences.com>
2022-03-02 15:25:52 -0700

    add rank icons

---
+++ src/components/teamScreen/visualTree/RankIcons.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const RankIcons = () => {
+  return (
+    <View style={{ flexDirection: 'row' }}>
+      <Text>RankIcons</Text>
+    </View>
+  );
+};
+
+export default RankIcons;
