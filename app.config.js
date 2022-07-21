7ce8277a76dfb62c6c1f65a5101283ff4ab27ef4
Ryan Seals <rseals@qsciences.com>
2022-07-21 17:04:00 -0600

    Move app.json to a javascript file for ease of use

---
+++ app.config.js
@@ -0,0 +1,157 @@
+// Staging settings
+let appName = 'q-connect-pro-staging';
+let appVersion = '0.0.111';
+let bundleIdent = 'com.qsciences.q-connect-pro-staging';
+let resClientID = 'com.googleusercontent.apps.348281014348-2ngcdn7mdg881n0ac1vvrpri6pdpbhjd';
+let googleServicesFileIos = './staging/GoogleService-Info.plist';
+let googleServicesFileAndroid = './staging/google-services.json';
+let versionCode = 110;
+let androidPackage = 'com.qsciences.q_connect_pro_staging';
+let firebaseAPIKey = 'AIzaSyDLvs6kf7lAlma9v714M_yF4EqP_UDIc5g';
+let authDomain = 'q-connect-pro-staging.firebaseapp.com';
+let projectID = 'q-connect-pro-staging';
+let storageBucket = 'q-connect-pro-staging.appspot.com';
+let senderID = '348281014348';
+let appID = '1:348281014348:web:33717b1f58b819a9594ce9';
+let measurementID = 'G-WV8PXGZQM1';
+let facebookAppId = '319892812842607';
+let facebookSchemeId = 'fb319892812842607';
+let facebookDisplayName = 'Q ConnectPro';
+
+// Production settings
+if(process.env.DEPLOY_ENVIRONMENT === 'production') {
+  appName = 'ambassador-resources-q';
+  appVersion = '2.2.3';
+  bundleIdent = 'com.qsciences.ambassadorResourcesQ';
+  resClientID = 'com.googleusercontent.apps.558665444400-qg6odikk6of0p60c53bi97ok7v20sceh';
+  googleServicesFileIos = './production/GoogleService-Info.plist';
+  googleServicesFileAndroid = './production/google-services.json';
+  androidPackage = 'com.qsciences.ambassadorResourcesQ';
+  versionCode = 20023;
+  firebaseAPIKey = 'AIzaSyCJ9fsDkv4R-P3Ok8ZYswXB5OfxZNiGXxg';
+  authDomain = 'q-innovation-prod.firebaseapp.com';
+  projectID = 'q-innovation-prod';
+  storageBucket = 'q-innovation-prod.appspot.com';
+  senderID = '558665444400';
+  appID = '1:558665444400:web:f5bc78feb12241c63551f5';
+  measurementID = 'G-EFX1G827C7';
+  facebookAppId = '172749824824779';
+  facebookSchemeId = 'fb172749824824779';
+  facebookDisplayName = 'Ambassador Resources Q';
+}
+
+export default {
+  "expo": {
+    "name": appName,
+    "slug": appName,
+    "version": appVersion,
+    "owner": "qsiholdingcompanyinc",
+    "orientation": "portrait",
+    "icon": "./assets/icon.png",
+    "scheme": "ambassador-resources-q",
+    "splash": {
+      "image": "./assets/splash.png",
+      "resizeMode": "contain",
+      "backgroundColor": "#000000"
+    },
+    "updates": {
+      "fallbackToCacheTimeout": 0,
+      "enabled": true
+    },
+    "assetBundlePatterns": ["**/*"],
+    "ios": {
+      "bundleIdentifier": bundleIdent,
+      "config": {
+          "googleSignIn": {
+              "reservedClientId": resClientID
+          }
+      },
+      "usesAppleSignIn": true,
+      "buildNumber": appVersion,
+      "googleServicesFile": googleServicesFileIos,
+      "supportsTablet": true,
+      "infoPlist": {
+          "CFBundleAllowMixedLocalizations": true,
+          "UIRequiresFullScreen": true
+      },
+      "requireFullScreen": true,
+      "usesIcloudStorage": true
+    },
+    "locales": {
+      "en": "./src/translations/iosPermissions/english.json",
+      "cs": "./src/translations/iosPermissions/czech.json",
+      "de": "./src/translations/iosPermissions/german.json",
+      "es": "./src/translations/iosPermissions/spanish.json",
+      "fr": "./src/translations/iosPermissions/french.json",
+      "it": "./src/translations/iosPermissions/italian.json",
+      "ja": "./src/translations/iosPermissions/japanese.json",
+      "nb": "./src/translations/iosPermissions/norwegian.json",
+      "nl": "./src/translations/iosPermissions/dutch.json"
+    },
+    "android": {
+      "package": androidPackage,
+      "versionCode": versionCode,
+      "googleServicesFile": googleServicesFileAndroid,
+      "softwareKeyboardLayoutMode": "pan",
+      "splash": {
+          "image": "./assets/splash.png",
+          "resizeMode": "contain",
+          "backgroundColor": "#000000",
+          "mdpi": "./assets/splash@1.png",
+          "hdpi": "./assets/splash@1.5x.png",
+          "xhdpi": "./assets/splash@2x.png",
+          "xxhdpi": "./assets/splash@3x.png",
+          "xxxhdpi": "./assets/splash@4x.png"
+      },
+      "adaptiveIcon": {
+          "foregroundImage": "./assets/foreground.png",
+          "backgroundImage": "./assets/background.png"
+      },
+      "permissions": [
+          "CAMERA",
+          "READ_CONTACTS",
+          "USE_FINGERPRINT",
+          "WRITE_EXTERNAL_STORAGE",
+          "READ_EXTERNAL_STORAGE"
+      ]
+    },
+    "androidNavigationBar": {
+      "barStyle": "light-content",
+      "backgroundColor": "#000000"
+    },
+    "web": {
+      "favicon": "./assets/favicon.png",
+      "config": {
+        "firebase": {
+          "apiKey": firebaseAPIKey,
+          "authDomain": authDomain,
+          "projectId": projectID,
+          "storageBucket": storageBucket,
+          "messagingSenderId": senderID,
+          "appId": appID,
+          "measurementId": measurementID
+        }
+      }
+    },
+    "facebookAppId": facebookAppId,
+    "facebookScheme": facebookSchemeId,
+    "facebookDisplayName": facebookDisplayName,
+    "packagerOpts": {
+      "config": "metro.config.js",
+      "sourceExts": [
+        "expo.ts",
+        "expo.tsx",
+        "expo.js",
+        "expo.jsx",
+        "ts",
+        "tsx",
+        "js",
+        "jsx",
+        "json",
+        "wasm",
+        "svg"
+      ]
+    },
+    "plugins": ["expo-tracking-transparency"]
+  }
+}
