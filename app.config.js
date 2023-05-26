9c74bf7e0e58c5cac697fe497c063c1a24a29001
Tim Knapp <tknapp@qsciences.com>
2023-05-26 10:36:26 -0600

    update version

--- app.config.js
+++ app.config.js
@@ -3,11 +3,11 @@ let uri = 'https://qservicesstaging-dev.azurewebsites.net/graphql';
 
 // Staging settings
 let appName = 'q-connect-pro-staging';
-let appVersion = '0.0.149';
+let appVersion = '0.0.150';
 let bundleIdent = 'com.qsciences.q-connect-pro-staging';
 let googleServicesFileIos = './staging/GoogleService-Info.plist';
 let googleServicesFileAndroid = './staging/google-services.json';
-let versionCode = 149;
+let versionCode = 150;
 let androidPackage = 'com.qsciences.q_connect_pro_staging';
 let firebaseAPIKey = 'AIzaSyDLvs6kf7lAlma9v714M_yF4EqP_UDIc5g';
 let authDomain = 'q-connect-pro-staging.firebaseapp.com';
